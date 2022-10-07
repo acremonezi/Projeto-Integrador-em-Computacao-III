04d57273acc3135abd731dc727913952ee5db04c
2005869 <2005869@aluno.univesp.br>
2022-10-07 20:53:47 -0300

    #32, remove delay and add time control

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -6,8 +6,14 @@
 #include "sensors/DHTxx.h"        // DHTxx Sensor Code
 #include "wifi/wifi.h"            // Wifi Connection
 #include "mqtt/mqtt.h"            // MQTT Code
-#include "display/display.h"      //Display Code
+#include "display/display.h"      // Display Code
 
+// Time
+#include <ctime>                  // To use in place of delay
+
+int timeStatus = 1;               // When 1, timeStart can be update
+time_t timeStart, timeNow;        // To control time elapsed
+int timeControl = 5;              // Time elapsed to new read
 
 void setup() {
 
@@ -21,10 +27,24 @@ void setup() {
 }
 
 void loop() {
- 
-  DHTxxRead();                    // DHTxx Sensor Readings
-  DHTxxSerialPrint();             // DHTxx Sensor Serial Print
-  mqttPublish();                  // MQTT Publish
-  refreshDisplay();               // Show MAC, temperature and humidity in display
+  if (timeStatus == 1)
+  {
+    timeStart = time(0);
+    timeNow = time(0);
+    timeStatus = 0;               // cant time update
+  }
+
+  if ((timeNow - timeStart) >= timeControl)
+  {
+    DHTxxRead();                    // DHTxx Sensor Readings
+    DHTxxSerialPrint();             // DHTxx Sensor Serial Print
+    mqttPublish();                  // MQTT Publish
+    refreshDisplay();               // Show MAC, temperature and humidity in display
+    timeStatus = 1;                 // can time update
+  }
+  
+  
   wifiReconnect();                // Wifi Reconnect
+
+  
 }

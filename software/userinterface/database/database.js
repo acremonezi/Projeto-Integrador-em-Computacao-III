937d88ea4ebdb56b3721e6807e44bd3310696cdf
2005869 <2005869@aluno.univesp.br>
2022-11-05 13:44:22 -0300

    #45, add sqlite

--- software/userinterface/database/database.js
+++ software/userinterface/database/database.js
@@ -1,3 +1,5 @@
+// InfluxDB
+
 const {InfluxDB} = require('@influxdata/influxdb-client')
 
 // You can generate an API token from the "API Tokens Tab" in the UI
@@ -8,6 +10,22 @@ const url = 'https://influxdb.bee.espertamente.com.br'
 
 const client = new InfluxDB({url: url, token: token})
 
+
+
+
+// SQLite3
+// For ubuntu, before install dependences or if you received a error in 'npm install sqlite3' do this:
+// 'sudo apt install node-sqlite' now you can do 'npm install sqlite3'
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './config.sqlite'
+})
+
+// Exports
+
 module.exports = {
-    client
+    client,
+    sequelize
 };

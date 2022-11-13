94c1b15b948a002f17a77c39ef0ce51bf1baa3a3
2005869 <2005869@aluno.univesp.br>
2022-11-13 09:45:12 -0300

    #44, set alias to host

--- software/userinterface/index.js
+++ software/userinterface/index.js
@@ -17,24 +17,67 @@ const app = express();
 const client = require('./database/database');
 const queries = require('./database/queries');
 const Hosts = require('./database/Hosts');
-
+const Host = require('./database/Hosts');
+const bodyParser = require('body-parser');
 // use ejs how view engine
 app.set('view engine', 'ejs');
 
 // use express to static files
 app.use(express.static('public'));
 
+//use express with body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 // Routes
+
+// main page
 app.get('/', (req, res) => {
     queries.allHosts((myHosts) => {
-        res.render('index', {
-            'myHosts': myHosts
+        Hosts.findAll({raw: true, order: [
+            ['host', 'ASC']
+        ]}).then(myHostsDB => {
+            res.render('index', {'myHosts': myHostsDB});
         });
+        
+        
     });
 });
 
 
+// open host config, to set alias
+app.get('/config/:id', (req, res) => {
+    var id = req.params.id;
+    
+    Hosts.findOne({
+        where: {id: id}
+    }).then(oneHost => {
+        if (oneHost != undefined){
+            res.render('config', {'oneHost': oneHost});
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+// set alias for individual host
+app.post('/saveconfig', (req, res) => {
+    var hostAlias = req.body.hostAlias;
+    var hostId = req.body.hostId;
+
+    console.log('chegou');
+    console.log(hostAlias);
+    console.log(hostId);
+
+    Hosts.update({alias: hostAlias}, {
+        where: {
+            id: hostId
+        }
+    });
+    res.redirect('/');
+    
+});
 
 // Server
 app.listen(8080, () => {

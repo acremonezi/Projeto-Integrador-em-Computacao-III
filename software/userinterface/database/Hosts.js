3cd5ed2d311cd0e01d80313ca2b72e71e02d6415
2005869 <2005869@aluno.univesp.br>
2022-11-23 23:05:14 -0300

    Closes #55, separate influxdb and sqlite3db in different files

--- software/userinterface/database/Hosts.js
+++ software/userinterface/database/Hosts.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const database = require('./database')
+const database = require('./sqlite3db')
 
 const Host = database.sequelize.define('Host', {
     host: {

f898ebf62c6161ea9b1ff4915758924810d2e545
2005869 <2005869@aluno.univesp.br>
2022-12-06 20:31:49 -0300

    #42, put data in tables

--- software/userinterface/database/queries.js
+++ software/userinterface/database/queries.js
@@ -41,26 +41,21 @@ function allHosts(callback) {
 
 function lastMeasure(id, callback) {
     var id = id;
-    
-    var listHumidity = [];
-    var dateHumidity = [];
-    var listTemperature = [];
-    var dateTemperature = [];
+    var dataHumidity = [];
+    var dataTemperature = [];
 
-    const fluxQuery = `from(bucket:"${client.bucket}") |> range(start: -30d, stop: -20d)
+    const fluxQuery = `from(bucket:"${client.bucket}") |> range(start: -30d, stop: -28d)
 |> filter(fn: (r) => r._measurement == "mqtt_consumer" and r.host == "8fa3519c28e3")`
 
     // This set avoids duplicate hosts
     client.queryApi.queryRows(fluxQuery, {
         next(row, tableMeta) {
-            const o = tableMeta.toObject(row)
+            const o = tableMeta.toObject(row)            
             if (o._field == "Humidity"){
-                listHumidity.push(o._value);
-                dateHumidity.push(o._time);
+                dataHumidity.push([o._time, o._value]);
             }
             if (o._field == "Temperature"){
-                listTemperature.push(o._value);
-                dateTemperature.push(o._time);
+                dataTemperature.push([o._time, o._value]);
             }
             console.log(
                 `${o._time} ${o._measurement} in '${o.location}' ${o._field}=${o._value} ${o._field}=${o._value}`)
@@ -71,8 +66,7 @@ function lastMeasure(id, callback) {
         },
         complete() {
             console.log('finish');
-            
-            callback(dateHumidity, listHumidity, dateTemperature, listTemperature);
+            callback(dataHumidity, dataTemperature);
         },
     })
 };
